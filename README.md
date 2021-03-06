d1c11817e719e5b282a3330f60bcfe24035244ce
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-03-05 23:50:35 -0600

    started melbaBot

---
+++ README.md
@@ -0,0 +1,17 @@
+# Melba Bot
+The bot i needed on my discord server!
+
+created in order to learn python and interact with my server.
+
+Ms Melba its a fictional character in colombia, its the lady that likes to gossip and 
+knows everything happening around her, thats where the inspiration comes from.
+
+## Avaliable Commands
+- !melbaTip <question> responds with a random answer, can be positive, negative or neutral
+- !cmds responds with a list of avaliable commands
+- !cleanupMelba deletes teh last 10 messages on the channel
+
+## Todo
+- !melbaGossip: brings news article
+- !cryptoMelba responds on the crypto channel with the current state of 3 crypto currencies (WIP)
+
---
+++ melba_Bot.py
@@ -0,0 +1,46 @@
+import os
+import random
+import discord
+from discord.ext import commands
+
+USER = os.getenv('MELBA_BOT')
+CRYPTO_C = os.getenv('CRYPTO_CHANNEL')
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+   print('We are logged in as {0.user}'.format(client))
+
+@client.event
+async def on_member_join(member):
+    print('hello new user')
+
+@client.command()
+async def cmds(ctx):
+    await ctx.send('Mijo, the commands are: \n!melbaTip <--- ask me anything mijo.\n!cryptoMelba <--- i tell you about crypto coins')
+
+@client.command()
+async def melbaTip(ctx, *, question):
+    responses = [
+        'Dont do it, mijo...',
+        'pailander... that means no',
+        'you should know',
+        'yeah, get that sweet sweet victory',
+        'do what you must',
+        'remember, with great power comes great responsability',
+        'Read the manual',
+        'Have you tried turning it on and off again',
+        'Go with the flow...',
+    ]
+    await ctx.send(f'Q: {question}\nA: {random.choice(responses)}')
+
+@client.command()
+async def cryptoMelba():
+    CRYPTO_CHANNEL = client.get_channel(CRYPTO_C)
+    await CRYPTO_CHANNEL.send('cryptos are on the rise mijo')
+
+@client.command()
+async def cleanupMelba(ctx, amount=10):
+    await ctx.channel.purge(limit=amount)
+
+client.run(USER)
