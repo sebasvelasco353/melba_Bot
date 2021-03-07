fd4560a4a8570127c0e832fb6f610cfa65fc3352
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-03-06 23:09:37 -0600

    added music playing functionality to Melba

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ knows everything happening around her, thats where the inspiration comes from.
 - !melbaTip <question> responds with a random answer, can be positive, negative or neutral
 - !cmds responds with a list of avaliable commands
 - !cleanupMelba deletes teh last 10 messages on the channel
+- !play <song-url> plays the song
+- !pause pauses the song
+- !stop stops the song
+- !resume resumes the song
 
 ## Todo
 - !melbaGossip: brings news article
 - !cryptoMelba responds on the crypto channel with the current state of 3 crypto currencies (WIP)
+- Make Melba be able to play multiple songs one after the other, now gives an error saying that its already connected
 
--- melba_Bot.py
+++ melba_Bot.py
@@ -1,6 +1,8 @@
 import os
 import random
 import discord
+import requests
+import youtube_dl
 from discord.ext import commands
 
 USER = os.getenv('MELBA_BOT')
@@ -42,5 +44,63 @@ async def cryptoMelba(ctx):
 @client.command()
 async def cleanupMelba(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Just chillin')
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+@client.command()
+async def leave(ctx):
+       voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+       if voice.is_connected():
+           await voice.disconnect()
+       else:
+           await ctx.send("Melba isnt in the server mijo.")
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("nothing playing mijo")
+
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused:
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused")
+
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
 
 client.run(USER)
